ccd6016c1d08b68e1b5397dcf642095a51bd081e
Tam Vo <vo.mita.ov@gmail.com>
2014-11-08 09:58:15 +0700

    Add position to posts

--- db/migrate/20141107183917_add_excerpt_monologue_post.rb
+++ db/migrate/20141107183917_add_excerpt_monologue_post.rb
@@ -1,5 +1,6 @@
 class AddExcerptMonologuePost < ActiveRecord::Migration
   def change
     add_column :monologue_posts, :excerpt, :string, limit: 1100
+    add_column :monologue_posts, :position, :integer, default: 1
   end
 end
